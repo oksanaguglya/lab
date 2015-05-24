7ef228ba4f606fb820a1fa71e9be7e3cd645cbf5
Andrei Navoichyk <a.navoichyk@softteco.com>
2015-05-24 14:06:47 +0300

    *

--- src/by/bsu/guglya/library/controller/LibraryServlet.java
+++ src/by/bsu/guglya/library/controller/LibraryServlet.java
@@ -3,23 +3,12 @@ package by.bsu.guglya.library.controller;
 import by.bsu.guglya.library.controller.command.*;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LibraryServlet extends HttpServlet {
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-    }
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        super.service(req, res);
-    }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
