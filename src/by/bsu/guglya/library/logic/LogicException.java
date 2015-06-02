2eaa78432d8e18ee4b9a423b1da640c261dce56a
Oksana Guglya <oksana.guglya@gmail.com>
2015-06-02 18:23:48 +0300

    LogicException added

---
+++ src/by/bsu/guglya/library/logic/LogicException.java
@@ -0,0 +1,20 @@
+package by.bsu.guglya.library.logic;
+
+public class LogicException extends Exception{
+
+    public LogicException() {
+    }
+
+    public LogicException(String message) {
+        super(message);
+    }
+
+    public LogicException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LogicException(Throwable cause) {
+        super(cause);
+    }
+
+}
